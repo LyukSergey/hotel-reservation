0fbc6d3bda6b47c4af83a1a1f0c361c3045cc82b
sergii.liuk <sergii.liuk@intellecteu.com>
2020-02-18 00:29:36 +0200

    Improve docker image building. Remove redundant configurations

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:8
+VOLUME /tmp
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} hotel-reservation.jar
+ENTRYPOINT ["java","-jar","/hotel-reservation.jar"]
+
--- docker_config/db/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO reservation (first_name, last_name, room_number, start_date, end_date)
-VALUES ('first_name1', 'last_name1', 1, '2020-02-11', '2020-02-13');
-INSERT INTO reservation (first_name, last_name, room_number, start_date, end_date)
-VALUES ('first_name2', 'last_name2', 2, '2020-02-12', '2020-02-18');
-INSERT INTO reservation (first_name, last_name, room_number, start_date, end_date)
-VALUES ('first_name3', 'last_name3', 3, '2020-02-13', '2020-02-21');
-INSERT INTO reservation (first_name, last_name, room_number, start_date, end_date)
-VALUES ('first_name4', 'last_name4', 4, '2020-02-14', '2020-02-22');
